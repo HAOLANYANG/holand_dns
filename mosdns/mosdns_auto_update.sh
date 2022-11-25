8b8160ac432f3c2d7e859eb22787ba35f0bac3c8
HAOLANYANG <1369799123@qq.com>
2022-11-25 19:51:55 +0800

    Update mosdns_auto_update.sh

--- mosdns/mosdns_auto_update.sh
+++ mosdns/mosdns_auto_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O /etc/mosdns/data/geoip.dat
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O /etc/mosdns/data/geosite.dat
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/cn.dat -O /etc/mosdns/data/cn.dat
@@ -10,9 +10,9 @@ wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/reject-list.txt -O /etc/mosdns/data/reject-list.txt
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/apple-cn.txt -O /etc/mosdns/data/apple-cn.txt
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt -O /etc/mosdns/data/gfw.txt
-wget --no-check-certificate https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/cdn_domain_list.txt -O /etc/mosdns/data/cdn_domain_list.txt
+wget --no-check-certificate https://cdn.jsdelivr.net/gh/pmkol/easymosdns@rules/cdn_domain_list.txt -O /etc/mosdns/data/cdn_domain_list.txt
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/geoip-only-cn-private.dat -O /etc/mosdns/geoip-only-cn-private.dat
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/HAOLANYANG/holand_dns/mosdns/rules/host.txt -O /etc/mosdns/data/host.txt
-wget --no-check-certificate https://fastly.jsdelivr.net/gh/pmkol/easymosdns@rules/gfw_ip_list.txt -O /etc/mosdns/data/gfw_ip_list.txt
+wget --no-check-certificate https://cdn.jsdelivr.net/gh/pmkol/easymosdns@rules/gfw_ip_list.txt -O /etc/mosdns/data/gfw_ip_list.txt
 systemctl restart mosdns
 
