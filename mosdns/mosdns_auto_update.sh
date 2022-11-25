e418408d1f26c8ef815e2073a1bc511d2d2205c3
HAOLANYANG <1369799123@qq.com>
2022-11-25 21:09:26 +0800

    Create mosdns_auto_update.sh

--- mosdns/mosdns_auto_update.sh
+++ mosdns/mosdns_auto_update.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O /etc/mosdns/data/geoip.dat
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O /etc/mosdns/data/geosite.dat
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/cn.dat -O /etc/mosdns/data/cn.dat
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/HAOLANYANG/holand_dns/mosdns/rules/ecs_cn_domain.txt -O /etc/mosdns/data/ecs_cn_domain.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/HAOLANYANG/holand_dns/mosdns/rules/ecs_tw_domain.txt -O /etc/mosdns/data/ecs_tw_domain.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/direct-list.txt -O /etc/mosdns/data/direct-list.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/proxy-list.txt -O /etc/mosdns/data/proxy-list.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/reject-list.txt -O /etc/mosdns/data/reject-list.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/apple-cn.txt -O /etc/mosdns/data/apple-cn.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt -O /etc/mosdns/data/gfw.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/pmkol/easymosdns@rules/cdn_domain_list.txt -O /etc/mosdns/data/cdn_domain_list.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/geoip-only-cn-private.dat -O /etc/mosdns/geoip-only-cn-private.dat
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/HAOLANYANG/holand_dns/mosdns/rules/host.txt -O /etc/mosdns/data/host.txt
-wget --no-check-certificate https://cdn.jsdelivr.net/gh/pmkol/easymosdns@rules/gfw_ip_list.txt -O /etc/mosdns/data/gfw_ip_list.txt
+wget --no-check-certificate https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /etc/mosdns/data/geoip.dat
+wget --no-check-certificate https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /etc/mosdns/data/geosite.dat
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt -O /etc/mosdns/data/direct-list.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt -O /etc/mosdns/data/proxy-list.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt -O /etc/mosdns/data/reject-list.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt -O /etc/mosdns/data/apple-cn.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt -O /etc/mosdns/data/gfw.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/HAOLANYANG/holand_dns/main/mosdns/rules/ecs_cn_domain.txt -O /etc/mosdns/data/ecs_cn_domain.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/HAOLANYANG/holand_dns/main/mosdns/rules/ecs_tw_domain.txt -O /etc/mosdns/data/ecs_tw_domain.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/HAOLANYANG/holand_dns/main/mosdns/rules/host.txt -O /etc/mosdns/data/host.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/pmkol/easymosdns/main/rules/cdn_domain_list.txt -O /etc/mosdns/data/cdn_domain_list.txt
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/pmkol/easymosdns/main/rules/gfw_ip_list.txt -O /etc/mosdns/data/gfw_ip_list.txt
 systemctl restart mosdns
-
